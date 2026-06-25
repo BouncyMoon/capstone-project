cf5b11929be8793b787e201460b89ce0f2198c4d
BouncyMoon <BouncyMoon@aol.com>
2026-06-25 00:16:40 -0500

    Add function to create and append tasks

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const list = document.getElementById("taskList");
 
+    if (input.value.trim() === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    list.appendChild(li);
+
+    input.value = "";
+}
